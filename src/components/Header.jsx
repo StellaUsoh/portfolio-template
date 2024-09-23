e4e97caccb3c60aba3dbc1d93d3464b44ee45de6
Lilianada <lilibest30.lb@gmail.com>
2024-09-23 11:53:02 +0100

    added and removed files from components

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { Container } from './Container'
+import ThemeToggle from './ThemeToggle'
+
+
+export function Header() {
+  return (
+    <>
+      <header
+        className="pointer-events-none relative z-50 flex flex-none flex-col"
+      >
+        <div
+          className="top-0 z-10 h-16 pt-6"
+        >
+          <Container
+            className="w-full"
+          >
+            <div className="">
+              <div className="flex justify-between items-center">
+              <div className="pointer-events-auto">
+                <a href="https://www.buildingbloques.com" className="hover:text-gray-400 font-semibold text-gray-900 dark:text-gray-100">
+                  Buy template
+                </a>
+                </div>
+                <div className="pointer-events-auto">
+                  <ThemeToggle />
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </header>
+    </>
+  )
+}
